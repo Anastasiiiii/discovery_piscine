cc10a79e8b63189455ca31d024af7dc45c99fe86
Anastasiia Fartushniak <74658637+Anastasiiiii@users.noreply.github.com>
2025-03-14 14:51:34 +0100

    Create i_got_that.py

---
+++ module3/ex02/i_got_that.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+input_message = input("What you gotta say? : ").strip()
+
+while input_message != "STOP":
+    input_message = input("I got that! Anything else? : ").strip()
