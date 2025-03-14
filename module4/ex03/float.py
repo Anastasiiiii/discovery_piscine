2edbe6a6e9756ae61eb4fb80a8bb5a3ff31cd394
Anastasiia Fartushniak <74658637+Anastasiiiii@users.noreply.github.com>
2025-03-14 14:48:53 +0100

    Create float.py

---
+++ module4/ex03/float.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+input_number = float(input("Give me a number: "))
+
+if input_number.is_integer():
+    print("This number is an integer.")
+else:
+    print("This number is a decimal.")
